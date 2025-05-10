7dc3af1ecf1a704b835be4b871713ddc3c2669f4
KDMV <franco_ojeda@soy.sena.edu.co>
2025-05-10 15:00:09 -0500

    realizando primer ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/F250680.py
@@ -0,0 +1,83 @@
+# Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+# con un numeral se indica un comnetario 
+
+# https://www.python.org
+
+# Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+"""
+este punto tiene como obetivo realizar 
+un comentario en varias lineas donde se explica 
+que para hacer el cometario se utilizan 3 comillas dobles
+
+"""
+
+'''
+este punto tiene como obetivo realizar 
+un comentario en varias lineas donde se explica 
+que para hacer el cometario se utilizan 3 comillas simples
+
+'''
+# Crea una variable (y una constante si el lenguaje lo soporta).
+
+# variables
+
+'''
+las variables son espacios de memoria que se utilizan para almacenar 
+valores.
+'''
+my_primerNombre = " FRANCO " 
+my_primerApellido = " OJEDA "
+
+# cosntantes 
+
+'''
+en python no existe las constantes, 
+pero podemos simularlos con letras mayusculas 
+y el uso de la palblra CONST
+'''
+
+MY_CONSTANTE = " OJEDA "
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+'''
+en pyton existen 4 tipos de datos primitivos 
+1. enteros
+2. flotantes o desimales
+3. booleanos
+4. cadena de texto
+se representan con la siguniente sintaxis
+
+'''
+my_int_años = 44
+my_float_peso = 71.83
+my_bool = True
+my_bool1 = False
+my_string = "estudiante en el SENA"
+my_other_string = " programa ADSO "
+
+# Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+"""
+para imprimir los valores de las variables.
+se utiliza la funcion print 
+
+"""
+print ("¡Hola, Python!")
+
+print(type(my_int_años))
+print(type(my_float_peso))
+print(type(my_bool))
+print(type(my_string))
+print(type(my_other_string))
+
+print(my_int_años)
+print(my_float_peso)
+print(my_bool)
+print(my_string)
+print(my_other_string)
+
+
+
